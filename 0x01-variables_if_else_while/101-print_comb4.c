64bc7d272775e15abf800c9271c6619e6188126d
Ken-Obieze <you@kenneth.obieze.com>
2022-07-01 21:18:55 -0700

    ad2

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int d, p, t;
+
+	for (d = '0'; d < '9'; d++)
+	{
+		for (p = d + 1; p <= '9'; p++)
+		{
+			for (t = p + 1; t <= '9'; t++)
+			{
+				if ((p != d) !=t)
+
+				{
+					putchar(d);
+					putchar(p);
+					putchar(t);
+
+					if (d == '7' && p == '8')
+						continue;
+
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
