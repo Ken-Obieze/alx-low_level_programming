9dba6ef277c320697cdfcee9141f4032c5f45329
Ken-Obieze <you@kenneth.obieze.com>
2022-07-01 20:27:52 -0700

    eg

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'z';
+
+	int i = 0;
+
+	/* your code goes there */
+	while (i < 26)
+	{
+		putchar(ch);
+		ch--;
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
