3a82c33eb17cf41e11529dd9711dfd55cc358fcd
Ken-Obieze <you@kenneth.obieze.com>
2022-07-06 16:16:02 -0700

    102

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -15,14 +15,19 @@ int main(void)
 	sum3 = 0;
 	sum5 = 0;
 
-	for (i = 0; i < 1024; i++)
+	for (i = 0; i < 1024; ++i)
 	{
 		if ((i % 3) == 0)
+		{
 			sum3 += i;
+		}
 		else if ((i % 5) == 0)
+		{
 			sum5 += i;
+		}
 	}
 	sum = sum3 + sum5;
 	printf("%lu\n", sum);
 	return (0);
 
+}
