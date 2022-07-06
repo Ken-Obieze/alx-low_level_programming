82500a95ba4ad2948266556f1a591c882a409ccc
Ken-Obieze <you@kenneth.obieze.com>
2022-07-05 18:07:04 -0700

    6

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the position of a number in number line
+ * @n: number to perform check on
+ * Return: 1, 0 or -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+
+	} else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+
+	} else if (n < 00
+	{
+		_putchar('-');
+		return (-1);
+
+	}
+
+}
+
