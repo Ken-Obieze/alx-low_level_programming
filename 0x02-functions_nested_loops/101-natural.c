12e554a13a6c501141e503de2fead91031faabf6
Ken-Obieze <you@kenneth.obieze.com>
2022-07-06 16:02:18 -0700

    102

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - computes summ of all multiples
+ * 
+ * Return: Always zero
+ */
+
+int main(void)
+{
+	unsigned long int sum, sum3, sum5;
+	int i;
+
+	sum = 0;
+	sum3 = 0;
+	sum5 = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0)
+			sum3 += i;
+		else if ((i % 5) == 0)
+			sum5 += i;
+	}
+	sum = sum3 + sum5;
+	printf("%lu\n", sum);
+	return (0);
+
