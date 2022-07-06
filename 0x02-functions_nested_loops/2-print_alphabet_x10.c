35087eb0654f63984c74f099f5bbc9935113ffff
Ken-Obieze <you@kenneth.obieze.com>
2022-07-05 17:23:08 -0700

    3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet ten times
+ *
+ * Return: Alwaays 0
+ */
+
+void print_alphabet_x10(void)
+{
+	for (int i = 0; i < 10; i++)
+		print_alphabet();
+}
