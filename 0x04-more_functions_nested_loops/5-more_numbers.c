f90592ac9483784905f04dc7c4413dd2e94c7af7
Ken-Obieze <you@kenneth.obieze.com>
2022-07-07 14:42:20 -0700

    sisth

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 to 14 ten times
+ * Return: Void
+ */
+
+void more_numbers(void)
+{
+	int c, d;
+
+	for (c = 0; c < 10; c++)
+		for (d = 0; d <= 14; d++)
+			_putchar(c + '0');
+		_putchar('\n');
+}
