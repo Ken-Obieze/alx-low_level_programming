eba589fd795ce28b54fe73e58fc1e52092ced2f2
Ken-Obieze <you@kenneth.obieze.com>
2022-07-07 14:55:41 -0700

    sisth

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,11 +10,9 @@ void more_numbers(void)
 	int c, d;
 
 	for (c = 0; c < 10; c++)
-		for (d = 0; d <= 14; d++)
-		{
-			if (d >= 10)
-				_putchar(d / 10 + '0');
-			_putchar(d % 10 + '0');
-		}
+	{
+		for (d = 0; d <= 14;)
+			_putchar(d);
 		_putchar('\n');
+	}
 }
