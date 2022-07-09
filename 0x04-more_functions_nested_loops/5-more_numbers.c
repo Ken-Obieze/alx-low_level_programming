2532b16e08452f97a0339c2c0d6e2bfd6ef5c70b
Ken-Obieze <you@kenneth.obieze.com>
2022-07-08 19:20:32 -0700

    a

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,18 +7,26 @@
 
 void more_numbers(void)
 {
+
 	int c, d;
 
 	for (c = 0; c < 10; c++)
 	{
+
 		for (d = 0; d <= 14; d++)
 		{
+
 			if (d >= 10 )
 			{
+
 				_putchar('1');
+
 			}
 			_putchar(d % 10 + '0');
+
 		}
+
 		_putchar('\n');
+
 	}
 }
