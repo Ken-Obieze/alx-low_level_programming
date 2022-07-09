808143b650ef2bab8653e186aaa275b9a731f3be
Ken-Obieze <you@kenneth.obieze.com>
2022-07-08 19:24:28 -0700

    a

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,16 +11,22 @@ void print_diagonal(int n)
 
 	if (n <= 0)
 	{
+
 		_putchar('$');
 		_putchar('\n');
+
 	}
 	else if (n > 0)
 	{
+
 		for (i = 0; i < n; i++)
 		{
+
 			for (j = 0; j <	i; j++)
 			{
+
 				_putchar(' ');
+
 			}
 			_putchar(92);
 			_putchar('$');
