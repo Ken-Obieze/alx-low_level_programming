5865bf617db93695b6a6133b6fcba7ef171d7bd9
Ken-Obieze <you@kenneth.obieze.com>
2022-07-12 13:28:08 -0700

    b

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps values of variables
+ * @a: first variabke to swap
+ * @b: second variabke to swap
+ */
+
+void swap_int(int *a, int *b);
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
