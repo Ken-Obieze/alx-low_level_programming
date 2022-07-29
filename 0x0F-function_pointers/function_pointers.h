1db4d62b26da5dd88504c68b067fafa9958440d9
Ken-Obieze <Kenneth.Obieze@outlook.com>
2022-07-29 02:27:55 +0000

    fix

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef FUNTION_POINTERS.H
-#define FUNTION_POINTERS.H
+#ifndef FUNC_H
+#define FUNC_H
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
