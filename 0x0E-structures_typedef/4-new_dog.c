20d16d10d40b9940558949fe32992fe1f1d6a577
Ken-Obieze <you@kenneth.obieze.com>
2022-08-02 21:48:37 -0700

    Struct_type_bulk

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - dog copy
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: pointer to new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int len1, len2;
+
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
+}
---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,22 @@
+# C - Structures, typedef
+
+## Task 0
+struct dog with the following elements:
+
+name, type = char *
+age, type = float
+owner, type = char *
+## Task 1
+a function that initialize a variable of type struct dog
+
+## Task 2
+a function that prints a struct dog
+
+## Task 3
+define a  new type dog_t as a new name for the type struct dog.
+
+## Task 4
+a function that creates a new dog.
+
+## Task 5
+a function that frees dogs.
