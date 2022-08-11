2f5df176bd502e970c06243441ef1c0c6d87b43b
Ken-Obieze <you@kenneth.obieze.com>
2022-08-11 16:50:20 -0700

    Run_first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * first - program prints sentence before executing main
+ */
+
+void first()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
