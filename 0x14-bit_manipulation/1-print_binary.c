435f93004a45668c7b25c20f806dd03ffe3d5999
Ken-Obieze <you@kenneth.obieze.com>
2022-08-18 20:25:49 -0700

    bit_manipulation_project

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,7 +7,7 @@
 
 void print_binary(unsigned long int n)
 {
-	int i, count = 0;
+	int i, cnt = 0;
 	unsigned long int j;
 
 	for (i = 63; i >= 0; i--)
@@ -17,10 +17,10 @@ void print_binary(unsigned long int n)
 		if (j & 1)
 		{
 			_putchar('1');
-			count++;
-		} else if (count)
+			cnt++;
+		} else if (cnt)
 			_putchar('0');
 	}
-	if (!count)
-		_putchar('0);
+	if (!cnt)
+		_putchar('0');
 }
