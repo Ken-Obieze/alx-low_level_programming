9e86d78188279db79174fa9775630fc32d1d5ad2
Ken-Obieze <you@kenneth.obieze.com>
2022-08-21 12:42:45 -0700

    fix8n9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,15 +9,16 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i, j;
-	int len = 0;
+	int i, j, len;
+
+	len = 0;
 
 	for (i = 0; src[i] != '\0'; i++)
 		len++;
 
 	for (j = 0; j < len; j++)
-		dest[i] = src[i];
-	dest[i] = '\0';
+		dest[j] = src[j];
+	dest[j] = '\0';
 
 	return (dest);
 }
