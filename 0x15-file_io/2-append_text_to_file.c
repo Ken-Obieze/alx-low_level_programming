16a9fcb9273ab79a68b363b352fa4c215d11b5fd
Ken-Obieze <you@kenneth.obieze.com>
2022-08-22 22:01:52 -0700

    bulk

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - function creates file
+ * @filename: for to read
+ * @text_content: text to be written to file
+ * Return: 1 or -1 for failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, t;
+	int s = 0;
+
+	if(!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd < 0)
+		return (-1);
+
+	if (text_content)
+	{
+		while (text_content[s])
+			s++;
+		t = write(fd, text_content, s);
+		if (t != s)
+			return (0);
+	}
+
+	close(fd);
+	return (1);
+}
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * main - copies content of filr to another
+ * @argc: number of arguments
+ * @argv: argument array
+ * Return: always zero success
+ */
+
+int main(int argc, char *argv[])
+{
+	int fdr, fdw, r, a, c;
+	char buf[BUFSIZ];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fdr = open(argv[1], O_RDONLY);
+	if (fdr < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fdw = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((r = read(fdr, buf, BUFSIZ)) > 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(fdr);
+		exit(99);
+	}
+	a = close(fdr);
+	b = close(fdw);
+	if (a < 0 || b < 0)
+	{
+		if (a < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdr);
+		if (b < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdw);
+		exit(100);
+	}
+	return (0);
+}
+		
