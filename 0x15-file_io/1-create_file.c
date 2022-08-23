3a527a41104c39723a657953488debc64f37fceb
Ken-Obieze <you@kenneth.obieze.com>
2022-08-22 22:07:50 -0700

    bulk

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	int fd, t;
 	int s = 0;
 
-if (!filename)
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
