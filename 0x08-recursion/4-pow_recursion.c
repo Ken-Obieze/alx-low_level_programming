748c4ceac74acefbdce5f472b1f8f226ba5bfaf9
Ken-Obieze <you@kenneth.obieze.com>
2022-08-26 15:43:53 -0700

    a

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * factorial - return factorial of number
+ * @n: number to calculate factorial
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		return (-1);
+	return (n * factorial(n -1));
+}
