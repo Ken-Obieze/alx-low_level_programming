0d7c093c30a7309d8576f4391e4866dc942405a3
Ken-Obieze <kenneth.obieze@outlook.com>
2022-08-27 02:58:03 +0100

    recurssion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,9 +8,9 @@
 
 int _strlen_recursion(char *s)
 {
-        if (*s == '\0')
-                return (0);
-        return (1 + _strlen_recursion(s + 1));
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
@@ -36,9 +36,9 @@ int is_palindrome(char *s)
 
 int check(char *s, int i, int len)
 {
-	if (*(s + i) != *(s + len -1))
+	if (*(s + i) != *(s + len - 1))
 		return (0);
 	if (i >= len)
-		return(1);
+		return (1);
 	return (check(s, i + 1, len - 1));
 }
