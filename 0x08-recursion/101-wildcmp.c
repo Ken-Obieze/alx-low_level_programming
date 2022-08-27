d46f6f5e97d7533d82a4fd3d3646d35c66dba173
Ken-Obieze <kenneth.obieze@outlook.com>
2022-08-27 03:12:21 +0100

    recurssion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -17,7 +17,7 @@ int wildcmp(char *s1, char *s2)
 	{
 		if (*s1 == '\0')
 			return (1);
-		return (wildcmp( s1 + 1, s2 + 1));
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	if (*s1 == '\0' || *s2 == '\0')
 		return (0);
@@ -29,7 +29,7 @@ int wildcmp(char *s1, char *s2)
 		if (*s1 == *s2)
 			sum += wildcmp(s1 + 1, s2 + 1);
 		sum += bsnatch(s1 + 1, s2);
-		return (!!sum);
+		return (sum);
 	}
 	return (0);
 }
@@ -59,7 +59,7 @@ int bsnatch(char *s1, char *s2)
 char *move(char *s2)
 {
 	if (*s2 == '*')
-		return (move(s2 + 1 ));
+		return (move(s2 + 1));
 	else
 		return (s2);
 }
