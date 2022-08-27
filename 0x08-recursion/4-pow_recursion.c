67e0e468da47cb8c1d37e437ad7acfa0e57f8344
Ken-Obieze <kenneth.obieze@outlook.com>
2022-08-27 02:33:33 +0100

    recurssion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,7 @@
 /**
  * _pow_recursion - return the value of x tothe power y
  * @x: the number to be raised
- * @y: the power to raise 
+ * @y: the power to raise
  * Return: result of operation or -1 if negative
  */
 
@@ -13,5 +13,5 @@ int _pow_recursion(int x, int y)
 		return (1);
 	if (y < 0)
 		return (-1);
-	return (x * _pow_recursion(x, y -1));
+	return (x * _pow_recursion(x, y - 1));
 }
