782060c1b201aa328ea7558e87d963caffe36bb7
Ken-Obieze <kenneth.obieze@outlook.com>
2022-09-05 09:32:02 +0100

    variadic_functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,13 +13,13 @@ int sum_them_all(const unsigned int n, ...)
 	va_list list;
 
 	if (n == 0)
-		return(0);
+		return (0);
 
 	va_start (list, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg (list, int);
+		sum += va_arg(list, int);
 
-	va_end (list);
+	va_end(list);
 	return (sum);
 }
