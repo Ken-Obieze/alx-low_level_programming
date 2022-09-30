147bf3ea195478c53f6ef404df2fd1ba06fb0e4a
Ken-Obieze <kenneth.obieze@outlook.com>
2022-09-30 06:00:11 +0100

    bulk

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 
-/** sum_dlistint - summs all data ina list
+/** 
+ * sum_dlistint - summs all data ina list
  * @head: pointer to list
  * Return: sum of data or 0 if list is empty
  */
@@ -18,5 +19,5 @@ int sum_dlistint(dlistint_t *head)
 		head = head->next;
 	}
 
-	return(sum);
+	return (sum);
 }
