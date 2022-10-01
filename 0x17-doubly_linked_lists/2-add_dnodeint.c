9b2752e9b40ae69badabc016e0cbd05677fe33ba
Ken-Obieze <kenneth.obieze@outlook.com>
2022-10-01 06:00:03 +0100

    fix

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newNode;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (NULL);
 
 	new = malloc(sizeof(dlistint_t));
