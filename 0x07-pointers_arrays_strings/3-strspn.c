fff23aabc8733ab98b2dc606484323f3828126f2
Ken-Obieze <kenneth.obieze@outlook.com>
2022-10-04 06:19:41 +0100

    fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
  /**
-  * _strspn - get the length of a prefix substring
+  * *_strspn - get the length of a prefix substring
   * @s: string
   * @accept: string with list of char
   * Return: number of bytes
