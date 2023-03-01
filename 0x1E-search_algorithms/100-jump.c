492758b42840a4c5386122d7a8a2aece395794a1
Ken-Obieze <kenneth.obieze@outlook.com>
2023-03-01 02:17:49 +0100

    task 100 fix

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -28,6 +28,13 @@ int jump_search(int *array, size_t size, int value)
 				printf("Value found between indexes [%lu] and [%lu]\n", str, end);
 				break;
 			}
+		} else
+		{
+			if (array[str] <= value)
+			{
+				printf("Value found between indexes [%lu] and [%lu]\n", str, end);
+				break;
+			}
 		}
 		str = end;
 		end = str + step;
