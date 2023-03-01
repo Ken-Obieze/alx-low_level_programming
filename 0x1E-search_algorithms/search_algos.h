2e5b00478061a939f34cd7a4cfa686e0768a8409
Ken-Obieze <kenneth.obieze@outlook.com>
2023-03-01 01:32:18 +0100

    fix

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,14 +7,12 @@
 #include <math.h>
 
 /**
- * struct skiplist_s - Singly linked list with an express lane
  * struct listint_s - singly linked list node structure
  * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
- * @express: Pointer to the next node in the express lane
  *
- * Description: singly linked list node structure with an express lane
+ * Description: singly linked list node structure
  */
 
 typedef struct listint_s
@@ -23,6 +21,15 @@ typedef struct listint_s
 	size_t index;
 	struct listint_s *next;
 } listint_t;
+
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane */
 
 typedef struct skiplist_s
 {
