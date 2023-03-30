157adffdb2502d7e859630f06e281506450c9ff8
Ken-Obieze <kenneth.obieze@outlook.com>
2023-03-31 00:45:24 +0100

    more_singly_linked Bulk commit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,18 +5,18 @@
  * @head: pointer to the first node of listint_t list.
  */
 
-void free_listint(listint_t *head)
+void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
 	if (head == NULL)
 		return;
 
-	while (head)
+	while (*head)
 	{
-		temp = head->next;
-		free(head);
-		head = temp;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
-	head = NULL;
+	*head = NULL;
 }
