7a1650f9cd9fe7385e9d12c885a671d4091cb3e3
Ken-Obieze <kenneth.obieze@outlook.com>
2023-03-31 00:37:57 +0100

    more_singly_linked Bulk commit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,14 +9,14 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
 
-	while (*head)
+	while (head)
 	{
 		temp = head->next;
 		free(head);
-		*head = temp;
+		head = temp;
 	}
-	*head = NULL;
+	head = NULL;
 }
